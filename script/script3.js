ae6e8d8766a590ae25e07cdf095e2dfad565ef8f
Moldy <48982601+tcpin@users.noreply.github.com>
2023-10-24 23:46:48 +0800

    Delete script directory

--- script/script3.js
+++
@@ -1,8 +0,0 @@
-function popupku() {
-  var body = document.querySelector("body");
-  var pWm = document.createElement("a");
-  pWm.setAttribute("href", "https://github.com/tcpin");
-  pWm.setAttribute("style", "text-decoration: none; color: white; opacity: .5; position: fixed; bottom : 10px; left: 10px");
-  pWm.innerHTML = "Github : Moldy";
-  body.appendChild(pWm);
-}
